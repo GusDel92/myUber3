e625f562e356485e7e1d25c316ba1c5e207881cb
GusDel92 <augustin.cuignet@student.ecp.fr>
2018-11-17 17:17:52 +0100

    MessageBox

--- MyUber3/src/myUberTools/Message.java
+++ MyUber3/src/myUberTools/Message.java
@@ -1,16 +1,36 @@
 
 package myUberTools;
 
+import myUberCustomer.Customer;
+
 public class Message {
+	
+	private int messageID;
 
+	static int messageCounter;
+	
 	public String text;
-
+	
+	public Message(String text) {
+		this.messageID=messageCounter;
+		messageCounter++;
+		this.text = text;
+	}
+	
 	public String getText() {
 		return text;
 	}
-
-	public void setText(String text) {
-		this.text = text;
-	}
+	
+	//j'hésite entre ces deux versions
+	
+	//public void sendMessage(Customer customer) {
+		//customer.getMessageBox().add(this);		
+	//}
+	
+//	public void sendMessage(Customer customer, String text) {
+	//	Message message = new Message(text);
+		//customer.getMessageBox().add(message);
+	//}
+	
 	
 }
