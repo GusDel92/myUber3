c58dd6e3c624b3b9ced463660a753a3f8d8f3ee2
Luca <lucatiebo@gmail.com>
2018-12-04 14:56:08 +0100

    fzf

---
+++ MyUber3/src/myUberCLItest/MoveCarCommand.java
@@ -0,0 +1,24 @@
+package myUberCLItest;
+
+import java.util.ArrayList;
+
+import myUberCar.Car;
+
+/**
+ * This method chages the position of a givent car. Attributes : car, latitude, longitude.
+ * @author Cuignet & Thiébaud
+ *
+ */
+
+public class MoveCarCommand {
+	
+	public static ArrayList<Car> main(String[] args){
+		int carID = Integer.parseInt(args[0]);
+		double longitude = Double.parseDouble(args[1]);
+		double latitude = Double.parseDouble(args[2]);
+		
+		
+		
+	}
+
+}
